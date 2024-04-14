f779a7e1026727f22c00ae59205ecf9d7081f900
zhao xinyuan <2292289473@qq.com>
2024-04-14 09:53:41 +0800

    feat: 新增历史放送页

--- miniprogram/components/second-nav-bar/second-nav-bar.ts
+++ miniprogram/components/second-nav-bar/second-nav-bar.ts
@@ -1,24 +1,22 @@
 // components/second-nav-bar/second-nav-bar.ts
 Component({
-
   /**
    * 组件的属性列表
    */
   properties: {
-
+    title: {
+      type: String,
+      value: "",
+    },
   },
 
   /**
    * 组件的初始数据
    */
-  data: {
-
-  },
+  data: {},
 
   /**
    * 组件的方法列表
    */
-  methods: {
-
-  }
-})
+  methods: {},
+});
--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,54 +1,117 @@
-// index.ts
-// 获取应用实例
-const app = getApp<IAppOption>()
-const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
+// pages/home/home.ts
 
-Component({
+import type Calendar from "../../types/bangumi-calendar";
+import type RequestResult from "../../types/request-result";
+
+Page({
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {
-      avatarUrl: defaultAvatarUrl,
-      nickName: '',
-    },
-    hasUserInfo: false,
-    canIUseGetUserProfile: wx.canIUse('getUserProfile'),
-    canIUseNicknameComp: wx.canIUse('input.type.nickname'),
+    scrollTop: 0,
+    // 返回 0-6，0 表示星期天，1 表示星期一，以此类推
+    dayOfWeek: null as number | null,
+    activeIndex: 0,
+    bangumiCalendar: [] as Calendar[],
   },
-  methods: {
-    // 事件处理函数
-    bindViewTap() {
-      wx.navigateTo({
-        url: '../logs/logs',
-      })
-    },
-    onChooseAvatar(e: any) {
-      const { avatarUrl } = e.detail
-      const { nickName } = this.data.userInfo
-      this.setData({
-        "userInfo.avatarUrl": avatarUrl,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    onInputChange(e: any) {
-      const nickName = e.detail.value
-      const { avatarUrl } = this.data.userInfo
-      this.setData({
-        "userInfo.nickName": nickName,
-        hasUserInfo: nickName && avatarUrl && avatarUrl !== defaultAvatarUrl,
-      })
-    },
-    getUserProfile() {
-      // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-      wx.getUserProfile({
-        desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-        success: (res) => {
-          console.log(res)
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    },
+
+  /**
+   * 日期选择回调
+   * @param event
+   */
+  calendarChange(event: any) {
+    const dataFromChild = event.detail.data;
+    this.setData({
+      scrollTop: 0,
+      activeIndex: dataFromChild,
+    });
   },
-})
+
+  /**
+   * 获取动漫日历
+   */
+  async getCalendarData() {
+    const that = this;
+    wx.request({
+      url:
+        "https://npm.onmicrosoft.cn/bangumi-database@0.0.11/dist/calendar.json",
+      success(res: RequestResult<Calendar[]>) {
+        const sunday = res.data.slice(6, 7);
+        const calendar: Calendar[] = [...sunday, ...res.data.slice(0, 6)];
+        console.log(calendar);
+
+        that.setData({
+          bangumiCalendar: calendar,
+        });
+      },
+    });
+  },
+
+  /**
+   * 获取今天是周几
+   */
+  getTodayOfWeek() {
+    // 返回 0-6，0 表示星期天，1 表示星期一，以此类推
+    const today = new Date().getDay();
+    this.setData({
+      dayOfWeek: today,
+    });
+  },
+
+  bangumiInit() {
+    this.setData({
+      activeIndex: this.data.dayOfWeek || 0,
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    wx.getSystemInfo({
+      success: (res) => {
+        this.setData({
+          homeHeight: `height: calc(${res.windowHeight}px - var(--height-navigation) - ${res.safeArea.top}px)`,
+        });
+      },
+    });
+    this.getCalendarData();
+    this.getTodayOfWeek();
+    this.bangumiInit();
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
