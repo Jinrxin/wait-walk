402f0904501584980e5c7226c0ede804dbdcb388
zhao xinyuan <2292289473@qq.com>
2024-04-20 10:21:04 +0800

    feat: 骨架屏加载

--- miniprogram/components/bangumi-card/bangumi-card.ts
+++ miniprogram/components/bangumi-card/bangumi-card.ts
@@ -16,10 +16,18 @@ Component({
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    loading: true,
+  },
 
   /**
    * 组件的方法列表
    */
-  methods: {},
+  methods: {
+    load() {
+      this.setData({
+        loading: false,
+      });
+    },
+  },
 });
